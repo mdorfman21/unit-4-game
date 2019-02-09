fac6c5455f61593951dca02e9df2381255e48d01
Morgan Dorfman <mdorfman21@gmail.com>
2019-02-09 09:39:52 -0500

    Multiple games after restart issue

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,46 @@
-//Initialize wins and losses
+(function() {
+  //Initialize wins and losses
+  var wins = 0;
+  var losses = 0;
 
-var wins = 0;
-var losses = 0;
+  function game() {
+    var points = 0;
+    var randomNumberToGuess = Math.floor(Math.random() * (200 - 19) + 19);
+    console.log(points);
+    $("#random-number-to-guess").text(
+      "Achieve this number: " + randomNumberToGuess
+    );
+    $("#wins").text("Your wins: " + wins);
+    $("#losses").text("Your losses: " + losses);
 
-//Function for initializing
-function initialize() {
-  $("#random-number-to-guess").text(
-    "Achieve this number: " + randomNumberToGuess()
-  );
-  $("#wins").text("Your wins: " + wins);
-  $("#losses").text("Your losses: " + losses);
-}
+    $("#image-one").removeAttr("value");
+    $("#image-two").removeAttr("value");
+    $("#image-three").removeAttr("value");
+    $("#image-four").removeAttr("value");
 
-function randomNumberToGuess() {
-  return Math.floor(Math.random() * (200 - 19) + 19);
-}
+    $("#image-one").attr("value", Math.floor(Math.random() * 12) + 1);
+    $("#image-two").attr("value", Math.floor(Math.random() * 12) + 1);
+    $("#image-three").attr("value", Math.floor(Math.random() * 12) + 1);
+    $("#image-four").attr("value", Math.floor(Math.random() * 12) + 1);
 
-function game() {
-  var points = 0;
-  initialize();
+    $("img").on("click", function(event) {
+      var element = event.target;
 
-  var imageOne = {
-    value: Math.floor(Math.random() * 12) + 1
-  };
-  var imageOneValue = Math.floor(Math.random() * 12) + 1;
-  console.log("image one value: " + imageOneValue);
-  var imageTwoValue = Math.floor(Math.random() * 12) + 1;
-  console.log("image two value: " + imageTwoValue);
-  var imageThreeValue = Math.floor(Math.random() * 12) + 1;
-  console.log("image three value: " + imageThreeValue);
-  var imageFourValue = Math.floor(Math.random() * 12) + 1;
-  console.log("image four value: " + imageFourValue);
+      points = +points + +element.getAttribute("value");
+      console.log(+points);
+      if (points === randomNumberToGuess) {
+        alert("You Win!!");
+        wins = wins + 1;
 
-  $("img").on("click", function() {
-    // alert(this);
-    // console.log($(this).val());
-    console.log(this.value);
-    // alert("You clicked " + this.value);
-  });
-}
+        game();
+      }
+      if (randomNumberToGuess < points) {
+        alert("You Lose!!");
+        losses = losses + 1;
 
-// function wins() {
-//   return (wins = 0);
-// }
-
-// function losses() {
-//   return (losses = 0);
-// }
-
-game();
+        game();
+      }
+    });
+  }
+  game();
+})();
